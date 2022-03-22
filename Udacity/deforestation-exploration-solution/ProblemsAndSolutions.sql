115e29a97bc3bcf3cf0e774a74ddc230472a601a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2022-03-22 08:17:47 +0100

    2. REGIONAL OUTLOOK
    Create a table that shows the Regions and their percent forest area (sum of forest area divided by sum of land area) in 1990 and 2016. (Note that 1 sq mi = 2.59 sq km).
    Based on the table you created, ....
    
    a. What was the percent forest of the entire world in 2016? Which region had the HIGHEST percent forest in 2016, and which had the LOWEST, to 2 decimal places?

--- Udacity/deforestation-exploration-solution/ProblemsAndSolutions.sql
+++ Udacity/deforestation-exploration-solution/ProblemsAndSolutions.sql
@@ -48,3 +48,24 @@ AND (total_area_sq_mi * 2.59) < 1324449
 ORDER BY total_area_sqkm DESC;
 
 /* 2. REGIONAL OUTLOOK  */
+
+DROP VIEW IF EXISTS region_to_forest;
+/* 2. Making New Table
+Create a table that shows the Regions and their percent forest area (sum of forest area divided by sum of land area) in 1990 and 2016. (Note that 1 sq mi = 2.59 sq km).
+Based on the table you created, ....  */
+
+CREATE VIEW region_to_forest AS
+SELECT f.country_name, f.year, f.forest_area_sqkm,
+l.total_area_sq_mi, r.region, r.income_group
+FROM forest_area f, land_area l, regions r
+WHERE (f.country_name = l.country_name
+AND f.country_name = r.country_name);
+
+SELECT * FROM region_to_forest;
+/* 2.2. What was the percent forest of the entire world in 2016? Which region had the HIGHEST percent forest in 2016, and which had the LOWEST, to 2 decimal places?  */
+SELECT Round(100.0*(rf.forest_area_sqkm / 
+  (rf.total_area_sq_mi * 2.59))::Numeric, 2) AS percentage
+FROM region_to_forest rf
+WHERE year = 2016
+AND rf.country_name = 'World'
+LIMIT 1;
